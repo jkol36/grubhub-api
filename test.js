931959d66d3f48f3cf76986e67514ca5ebc82a80
Jonathan Kolman <jonathankolman@Jonathans-MBP.fios-router.home>
2021-08-13 09:49:32 -0400

    auto complete works

--- test.js
+++ test.js
@@ -14,34 +14,15 @@ const searchListing = (
   .set(headers)
   .then(res => res)
 }
+
+
 describe('tests', () => {
   let accessToken = null
-  it('should search for pizza near me', done => {
-    let location = `POINT(-75.23348237%2039.99370956)`
-    let locationMode = 'DELIVERY'
-    let orderMethod = 'delivery'
-    let pageSize = 20
-    let headers =  {
-        'authority': 'api-gtm.grubhub.com',
-        'cache-control': 'max-age=0',
-        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-        'dnt': '1',
-        'perimeter-x': 'eyJ1IjoiODhmMDEyYTAtZjk2NS0xMWViLTk0ODUtMDUwNTVkNGU3YjAxIiwidiI6IjNjODQyNDgyLWY3ZjEtMTFlYi05NjBiLTAyNDJhYzEyMDAxYSIsInQiOjE2Mjg1NTA1MDY4MjAsImgiOiI3NWI1NmM5ZTcxNTk3MTE5ZjIwMWNiOWJjNTAzMWJhMjlmODkzZmMyMWYyOGQ4MDFmYTUzZTM0MGQ1NzhlMWI0In0=',
-        'sec-ch-ua-mobile': '?0',
-        'authorization': 'Bearer 8daf0be0-8d85-4bdb-b0b2-13b22997e72f',
-        'accept': 'application/json',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-        'if-modified-since': '0',
-        'origin': 'https://www.grubhub.com',
-        'sec-fetch-site': 'same-site',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'referer': 'https://www.grubhub.com/',
-        'accept-language': 'en-US,en;q=0.9'
-    }
-    searchListing(orderMethod, locationMode, pageSize, location, headers)
+  let headers = null
+  it.only('shoud loaad proxies', done => {
+    agent.get("https://raw.githubusercontent.com/scidam/proxy-list/master/proxy.json")
     .then(res => {
-      console.log(res)
+      console.log(res.body)
       done()
     })
   })
@@ -67,7 +48,7 @@ describe('tests', () => {
 
   })
   it.only('should search listings using access token', done => {
-    const headers = {
+    headers = {
         'authority': 'api-gtm.grubhub.com',
         'cache-control': 'max-age=0',
         'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
@@ -86,7 +67,8 @@ describe('tests', () => {
         'accept-language': 'en-US,en;q=0.9'
     };
 
-    agent.get('https://api-gtm.grubhub.com/diners/783587cc-487f-11e4-9697-9cb654858910/search_listing?pageNum=1&pageSize=10&facet=scheduled%3Afalse&facet=orderType%3AALL&sorts=default')
+    agent
+    .get('https://api-gtm.grubhub.com/diners/783587cc-487f-11e4-9697-9cb654858910/search_listing?pageNum=1&pageSize=10&facet=scheduled%3Afalse&facet=orderType%3AALL&sorts=default')
     .set(headers)
     .then(res => {
       console.log(res.body)
@@ -96,4 +78,17 @@ describe('tests', () => {
     })
     .catch(done)
   })
+
+  it.only('should autocomplete', done => {
+    let queryText = '1200 market street'
+    agent.get('https://api-gtm.grubhub.com/geocode/autocomplete?queryText=${queryText}&locationBias=true')
+    .set(headers)
+    .then(res => {
+      console.log(res.body)
+      const { results } = res.body
+      expect(results).to.be.an.array
+      done()
+    })
+  })
+
 })
